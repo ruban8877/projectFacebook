880136989aa802223b033647ae9f38c51b6a8286
ruban8877 <semalaruban.s@gmail.com>
2021-03-24 01:32:44 -0700

    change text

--- src/org/sample/ArrayTask.java
+++ src/org/sample/ArrayTask.java
@@ -4,25 +4,13 @@ import java.util.Arrays;
 
 public class ArrayTask {
 	public static void main(String[] args) {
-//		int[] a =new int[] {1,5,4,9,8};
-//	Arrays.sort(a);
-//	
-//	for (int i = 0; i <=4; i++) {
-//		
-//		System.out.println(a[i]);
-//	}
-//	
-//		String s = "pradhap";
-//		String s1="";
-//		
-//		for (int i = s.length()-2; i >=0; i--) {
-//			char c = s.charAt(i);
-//			s1=s1+c;
-//		}
-//		System.out.println(s1);
-//				
-				
-		String s="java";
+		int[] a =new int[] {1,5,4,9,8};
+	Arrays.sort(a);
+	
+	for (int i = 0; i <=4; i++) {
+		
+		System.out.println(a[i]);
+	}	
 		
 				
 				
